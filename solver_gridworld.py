27393bb56a23e549da32face130662a5a838bbd8
Niket <40545971+npnike10@users.noreply.github.com>
2023-03-02 03:22:51 -0600

    Added Value iteration to solver

--- solver_gridworld.py
+++ solver_gridworld.py
@@ -9,7 +9,7 @@ class Policy:
         self.num_states=num_states
 
     def initialise(self):
-        for s in self.num_states:
+        for s in range(self.num_states):
             self.policy[s]=random.randrange(self.num_actions)
     
     def epsilon_greedy(self,state,eps,q_table):
@@ -20,6 +20,14 @@ class Policy:
             return random.randrange(self.num_actions)
         else:
             return np.argmax(self.q[state])
+
+    def greedy(self,s,value_table,agent=None):
+        if len(np.asarray(value_table).shape)==1:
+            v=value_table
+            self.policy[s]=np.argmax(np.asarray([sum([agent.env.p(s1,s,act)*(agent.env.r(s,act)+agent.gamma*v[s1]) for s1 in range(agent.env.num_states)]) for act in range(agent.env.num_actions)]))
+        else:
+            self.q=value_table
+            self.policy[s]=np.argmax(self.q[s])
     
     def get_action(self,s):
         return self.policy[s]
@@ -43,7 +51,7 @@ class PolicyIteration:
             delta=0
             for st in range(self.env.num_states):
                 value=self.v[st]
-                action=self.pi[st]
+                action=self.pi.get_action(st)
                 self.v[st]=sum([self.env.p(next_st,st,action)*(self.env.r(st,action)+self.gamma*self.v[next_st]) for next_st in range(self.env.num_states)])
                 delta=max(delta,abs(value-self.v[st]))
             if delta<self.theta:
@@ -51,12 +59,12 @@ class PolicyIteration:
     
     def improve_policy(self):
         for st in range(self.env.num_states):
-            self.pi[st]=np.argmax(np.asarray([sum([self.env.p(next_st,st,act)*(self.env.r(st,act)+self.gamma*self.v[next_st]) for next_st in range(self.env.num_states)]) for act in range(self.env.num_actions)]))
+            self.pi.set_action(st,np.argmax(np.asarray([sum([self.env.p(next_st,st,act)*(self.env.r(st,act)+self.gamma*self.v[next_st]) for next_st in range(self.env.num_states)]) for act in range(self.env.num_actions)])))
 
     def is_optimal(self,old_pi):
         optimal=True
         for st in range(self.env.num_states):
-            if self.pi[st]!=old_pi[st]:
+            if self.pi.get_action(st)!=old_pi[st]:
                 optimal=False
         return optimal
     
@@ -64,7 +72,34 @@ class PolicyIteration:
         optimal=False
         while not optimal:
             self.evaluate_policy()
-            old_pi=self.pi
+            old_pi=self.pi.policy.copy()
             self.improve_policy()
             optimal=self.is_optimal(old_pi)
+        return self.v, self.pi
+
+class ValueIteration:
+
+    def __init__(self,theta,gamma,env):
+        self.theta=theta
+        self.gamma=gamma
+        self.env=env
+        self.v=random.sample(range(-100,100), k=env.num_states)
+        self.pi=Policy(env.num_states, env.num_actions)
+    
+    def update_value(self):
+        while True:
+            delta=0
+            for st in range(self.env.num_states):
+                value=self.v[st]
+                action=self.pi.get_action(st)
+                self.v[st]=max([sum([self.env.p(next_st,st,action)*(self.env.r(st,action)+self.gamma*self.v[next_st]) for next_st in range(self.env.num_states)]) for act in range(self.env.num_actions)])
+                delta=max(delta,abs(value-self.v[st]))
+            if delta<self.theta:
+                break
+    
+    def learn_policy(self):
+        self.update_value()
+        for s in range(self.env.num_states):
+            self.pi.greedy(s,self.v,self)
+        return self.v, self.pi
     
---
+++ train_gridworld.py
@@ -0,0 +1,55 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import gridworld
+from solver_gridworld import PolicyIteration, ValueIteration
+
+def main():
+    # Create environment
+    env = gridworld.GridWorld(hard_version=False)
+
+    # constants and parameters
+    theta=0.0005
+    gamma=0.95
+
+    # Initialize simulation
+    s = env.reset()
+
+    # Policy Iteration
+    PI_agent=PolicyIteration(theta,gamma,env)
+    PI_values,PI_policy=PI_agent.learn_policy()
+    print('Policy Iteration:',PI_values,PI_policy.policy)
+
+    # Value Iteration
+    VI_agent=ValueIteration(theta,gamma,env)
+    VI_values,VI_policy=VI_agent.learn_policy()
+    print('Value Iteration:',VI_values,VI_policy.policy)
+
+    # Create log to store data from simulation
+    log = {
+        't': [0],
+        's': [s],
+        'a': [],
+        'r': [],
+    }
+
+    # Simulate until episode is done
+    done = False
+    while not done:
+        a = random.randrange(4)
+        (s, r, done) = env.step(a)
+        log['t'].append(log['t'][-1] + 1)
+        log['s'].append(s)
+        log['a'].append(a)
+        log['r'].append(r)
+
+    # Plot data and save to png file
+    plt.plot(log['t'], log['s'])
+    plt.plot(log['t'][:-1], log['a'])
+    plt.plot(log['t'][:-1], log['r'])
+    plt.legend(['s', 'a', 'r'])
+    plt.savefig('figures/gridworld/test_gridworld.png')
+
+
+if __name__ == '__main__':
+    main()
